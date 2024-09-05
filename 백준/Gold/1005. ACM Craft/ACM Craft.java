800bf1b482f8f03100957a34a2eeb73c6f77eba6
김기창 <156048545+kgc9007@users.noreply.github.com>
2024-09-05 12:37:13 +0900

    [Gold III] Title: ACM Craft, Time: 684 ms, Memory: 242728 KB -BaekjoonHub

---
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -0,0 +1,85 @@
+// BOJ 1005번 ACM Craft
+// https://www.acmicpc.net/problem/1005
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+		while (T-- > 0) {
+			st = new StringTokenizer(br.readLine());
+
+			int N = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+
+			int[] buildings = new int[N + 1];
+			int[] inDegree = new int[N + 1];
+			int[] visited = new int[N + 1];
+			List<Integer>[] adj = new ArrayList[N + 1];
+			for (int i = 1; i <= N; i++) {
+				adj[i] = new ArrayList<Integer>();
+			}
+
+			st = new StringTokenizer(br.readLine());
+			for (int i = 1; i <= N; i++) {
+				buildings[i] = Integer.parseInt(st.nextToken());
+			}
+
+			for (int i = 0; i < K; i++) {
+				st = new StringTokenizer(br.readLine());
+
+				int start = Integer.parseInt(st.nextToken());
+				int end = Integer.parseInt(st.nextToken());
+
+				adj[start].add(end);
+				inDegree[end]++;
+			}
+
+			int W = Integer.parseInt(br.readLine());
+
+			int max = 0;
+			int sum = 0;
+			Queue<int[]> queue = new LinkedList<>();
+			for (int i = 1; i <= N; i++) {
+				if (inDegree[i] == 0) {
+					visited[i] = buildings[i];
+					queue.add(new int[] { i, visited[i] });
+				}
+			}
+
+			while (!queue.isEmpty()) {
+				int[] curr = queue.poll();
+
+				if (curr[0] == W) {
+					max = Math.max(max, curr[1]);
+					break;
+				}
+
+				for (int i : adj[curr[0]]) {
+					inDegree[i]--;
+					visited[i] = Math.max(visited[i], curr[1] + buildings[i]);
+
+					if (inDegree[i] == 0) {
+						queue.add(new int[] { i, visited[i] });
+					}
+				}
+			}
+
+			sb.append(max).append('\n');
+		}
+
+		// 결과 출력
+		System.out.println(sb);
+	}
+}
