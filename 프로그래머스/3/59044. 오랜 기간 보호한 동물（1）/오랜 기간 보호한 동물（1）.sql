a9c716f8893bfd3fed52f49fbcf90613b051b36f
김기창 <156048545+kgc9007@users.noreply.github.com>
2024-09-10 16:42:18 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT ins.NAME, ins.DATETIME
+FROM 
+    ANIMAL_INS ins 
+    LEFT JOIN 
+    ANIMAL_OUTS outs
+ON ins.ANIMAL_ID = outs.ANIMAL_ID
+WHERE 
+    outs.DATETIME IS NULL
+ORDER BY ins.DATETIME ASC
+LIMIT 3;
